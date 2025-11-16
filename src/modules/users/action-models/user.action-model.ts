5cc600228eb12337102e7af8d63db53c89db1c09
Heba-Webdev23 <jtvonline@yahoo.com>
2025-11-16 12:32:16 +0300

    feat: create model action

---
+++ src/modules/users/action-models/user.action-model.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { AbstractModelAction } from '@shared/abstract-model-action';
+import { User } from '../models/user.model';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserModelAction extends AbstractModelAction<User> {
+  constructor(
+    @InjectRepository(User)
+    repository: Repository<User>,
+  ) {
+    super(repository, User);
+  }
+}
