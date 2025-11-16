9e3b150db316519afa235574c4283926498f1763
Heba-Webdev23 <jtvonline@yahoo.com>
2025-11-16 12:32:04 +0300

    feat: create user

---
+++ src/modules/users/users.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import UserCoreService from './services/user-core.service';
+import UserValidationService from './services/user-validation.service';
+import { UserModelAction } from './action-models/user.action-model';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './models/user.model';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [
+    UsersService,
+    UserCoreService,
+    UserValidationService,
+    UserModelAction,
+  ],
+  exports: [UsersService],
+})
+export class UsersModule {}
