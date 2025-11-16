2f5796d567cf95631988feb81ea0ffb213eed572
Heba-Webdev23 <jtvonline@yahoo.com>
2025-11-16 12:28:58 +0300

    chore: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  SwaggerModule.setup('api/docs', app, SwaggerModule.createDocument(app, config));
+  SwaggerModule.setup(
+    'api/docs',
+    app,
+    SwaggerModule.createDocument(app, config),
+  );
 
   const port = process.env.PORT ?? 4001;
   await app.listen(port);
