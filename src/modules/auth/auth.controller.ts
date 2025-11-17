a4a95176f598468269848bc7dc864477085986e0
Heba Omar <jtvonline@yahoo.com>
2025-11-17 14:59:54 +0300

    Revert "feat: Implement user login endpoint with local authentication"

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,8 +3,6 @@ import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { RegisterBodyValidator } from './validators/register.validator';
 import { RegisterDocs } from './docs/register.doc';
-import { LoginBodyValidator } from './validators/login.validator';
-import { LoginDocs } from './docs/login.doc';
 
 @Controller('auth')
 @ApiTags('Authentication')
@@ -17,12 +15,5 @@ export class AuthController {
   @RegisterDocs.register()
   async createNewUser(@Body() user: RegisterBodyValidator) {
     return await this.authService.registerWithEmailAndPassword(user);
-  }
-
-  @HttpCode(200)
-  @Post('/login')
-  @LoginDocs.login()
-  async loginUser(@Body() credentials: LoginBodyValidator) {
-    return await this.authService.login(credentials);
   }
 }
--- src/modules/auth/validators/login.validator.ts
+++
@@ -1,3 +0,0 @@
-import { LoginDto } from '../dtos/login.dto';
-
-export class LoginBodyValidator extends LoginDto {}
