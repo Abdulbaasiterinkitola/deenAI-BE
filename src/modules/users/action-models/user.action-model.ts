c9879ea8a8d59d3b9c4a9207169eff276e579f27
Heba-Webdev23 <jtvonline@yahoo.com>
2025-11-19 16:30:07 +0300

    fix: remove direct usage of typeorm

--- src/modules/users/action-models/user.action-model.ts
+++ src/modules/users/action-models/user.action-model.ts
@@ -12,15 +12,4 @@ export class UserModelAction extends AbstractModelAction<User> {
   ) {
     super(repository, User);
   }
-
-  async getById(id: string) {
-    return await this.get({ id });
-  }
-
-  async updatePassword(id: string, password: string) {
-    return await this.update({
-      updatePayload: { password },
-      identifierOptions: { id },
-    });
-  }
 }
