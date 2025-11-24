a6862395d79cbe7c4ec4ad76d333db6f1b8b6066
Mustapha Ridwan <mustapharid19@gmail.com>
2025-11-24 18:21:35 +0100

    feat(chats): include token usage stats in message response

--- src/modules/chats/docs/chats.doc.ts
+++ src/modules/chats/docs/chats.doc.ts
@@ -112,6 +112,11 @@ export class ChatsDocs {
                 createdAt: '2025-01-01T12:00:05.000Z',
                 updatedAt: '2025-01-01T12:00:05.000Z',
               },
+              usage: {
+                inputTokens: 50,
+                outputTokens: 100,
+                totalTokens: 150,
+              },
             },
           },
         },
