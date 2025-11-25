eab399634f6c9f83b2d001262906c3136ae42e32
Sherif Ibrahim <ibrahimsherifdeji@gmail.com>
2025-11-25 22:26:32 +0100

    fix: resolve CI build failures

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,7 +6,6 @@ import {
   Body,
   UsePipes,
   ValidationPipe,
-  Patch,
 } from '@nestjs/common';
 import { Controller, Post, Patch, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
@@ -69,14 +68,13 @@ export class UsersController {
    */
   @Patch('me/pause')
   @HttpCode(HttpStatus.OK)
-  // You can add API documentation here later if you use Swagger
   async pauseAccount(@AuthUser() user: User) {
     const pausedUser = await this.usersService.pauseAccount(user.id);
     return {
       status_code: HttpStatus.OK,
       message: 'Account successfully paused',
       data: {
-        user: this.usersService.getUserProfile(pausedUser),
+        user: pausedUser,
       },
     };
   }
@@ -86,14 +84,13 @@ export class UsersController {
    */
   @Patch('me/reactivate')
   @HttpCode(HttpStatus.OK)
-  // You can add API documentation here later if you use Swagger
   async reactivateAccount(@AuthUser() user: User) {
     const reactivatedUser = await this.usersService.reactivateAccount(user.id);
     return {
       status_code: HttpStatus.OK,
       message: 'Account successfully reactivated',
       data: {
-        user: this.usersService.getUserProfile(reactivatedUser),
+        user: reactivatedUser,
       },
     };
   }
