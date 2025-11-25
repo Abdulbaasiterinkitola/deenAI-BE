d9c05755a33bdfafb0cea3eb912f0c52d57ee7d7
Mustapha Ridwan <mustapharid19@gmail.com>
2025-11-25 21:58:06 +0100

    fix conflict

--- src/database/migrations/1892000000002-add-token-limit-to-plans.ts
+++ src/database/migrations/1892000000002-add-token-limit-to-plans.ts
@@ -4,7 +4,7 @@ export class AddTokenLimitToPlans1892000000002 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "plans" ADD "token_limit" integer`);
     await queryRunner.query(
-      `UPDATE "plans" SET "token_limit" = 5000 WHERE "slug" = 'free'`,
+      `UPDATE "plans" SET "token_limit" = 100000 WHERE "slug" = 'free'`,
     );
   }
 
