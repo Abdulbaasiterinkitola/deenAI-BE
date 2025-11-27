a5de7ddb1505f1b8d28f8fa5a03c108f1536935b
Heba-Webdev23 <jtvonline@yahoo.com>
2025-11-27 09:37:07 +0300

    fix: feedback migration

--- src/database/migrations/1764210192378-feedback.ts
+++ src/database/migrations/1764210192378-feedback.ts
@@ -5,35 +5,19 @@ export class Feedback1764210192378 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      CREATE TABLE "feedbacks" (
+      CREATE TABLE IF NOT EXISTS feedbacks (
         "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-        "created_at" TIMESTAMP NOT NULL DEFAULT now(),
-        "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
         "name" character varying(255) NOT NULL,
         "title" character varying(255) NOT NULL,
         "description" text NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
         CONSTRAINT "PK_79affc530fdd838a9f1e0cc30be" PRIMARY KEY ("id")
       )
-    `);
-
-    await queryRunner.query(`
-      ALTER TABLE "users"
-      ADD COLUMN IF NOT EXISTS "name" character varying
-    `);
-
-    await queryRunner.query(`
-      UPDATE "users"
-      SET "name" = 'Anonymous'
-      WHERE "name" IS NULL
-    `);
-
-    await queryRunner.query(`
-      ALTER TABLE "users"
-      ALTER COLUMN "name" SET NOT NULL
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "feedbacks"`);
+    await queryRunner.query(`DROP TABLE IF EXISTS feedbacks;`);
   }
 }
