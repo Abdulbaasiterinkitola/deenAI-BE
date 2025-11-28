774a1e977af4fb97c764fe2c092dc7d33c8850f8
heba-webdev <jtvonline@yahoo.com>
2025-11-28 08:58:47 +0300

    chore: add min and max length

--- src/modules/chats/dtos/rename-chat.dto.ts
+++ src/modules/chats/dtos/rename-chat.dto.ts
@@ -1,7 +1,9 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, Min, MinLength, MaxLength } from 'class-validator';
 
 export class RenameChatDto {
   @IsString()
   @IsNotEmpty()
+  @MinLength(8, { message: 'Title must be at least 8 characters long' })
+  @MaxLength(200, { message: 'Title must not exceed 200 characters' })
   title: string;
 }
