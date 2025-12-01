d39aa2e1df2a0adf1cb41f02921bded871667594
heba-webdev <jtvonline@yahoo.com>
2025-12-01 20:39:51 +0300

    fix: rate limit

--- src/modules/email/email.controller.ts
+++ src/modules/email/email.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { Throttle } from '@nestjs/throttler';
 import { EmailService } from './email.service';
 import { EmailDto } from './dtos/email.dto';
 import { Public } from '../../guards/public.decorator';
 
 @Controller()
+@Throttle({
+  default: { limit: 3, ttl: 60 * 1000 }, // 3 requests per minute
+})
 export class EmailServiceController {
   constructor(private readonly emailServiceService: EmailService) {}
 
